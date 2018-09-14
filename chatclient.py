de081ce0687c64eb221bf3083dfbee5ef7d4e00b
Nicole Traschikoff <tricoleromina@ciencias.unam.mx>
2018-09-14 15:56:18 -0700

    Subiendo esqueleto de cliente y servidor

---
+++ chatclient.py
@@ -0,0 +1,9 @@
+import socket
+#clase para simular el cliente
+
+class chatclient:
+
+    def __init__(self):
+
+
+    def run(self):
---
+++ chatserver.py
@@ -0,0 +1,9 @@
+import socket
+
+#clase para simular un servidor
+class chatserver:
+
+#crea un socket y lo mantiene en escucha
+    def __init__(self):
+
+    def run(self):
