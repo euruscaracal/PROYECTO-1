ab7136eba4930c973b205fb374ecc5272e1d2222
Nicole Traschikoff <tricoleromina@ciencias.unam.mx>
2018-09-14 16:14:06 -0700

    Estableciendo conexion entre servidor y cliente

--- chatclient.py
+++ chatclient.py
@@ -1,9 +1,28 @@
 import socket
-#clase para simular el cliente
 
+host = "127.0.0.1"
+port = 6666
+
+#clase que simula el cliente
 class chatclient:
 
     def __init__(self):
 
+        self.soquete = socket.socket()
+        self.soquete.connect((host, port))
 
     def run(self):
+        datos = self.soquete.recv(4096)
+        print (datos.decode('utf-8'))
+
+        while True:
+          message = input("envia un mensaje: ")
+          self.soquete.send(message.encode('utf-8'))
+
+          if message == "quit":
+            print("bye")
+            self.soquete.close()
+            break
+
+S = chatclient()
+S = S.run()
--- chatserver.py
+++ chatserver.py
@@ -1,9 +1,39 @@
 import socket
 
 #clase para simular un servidor
+host = "127.0.0.1"
+port = 6666
+
 class chatserver:
 
-#crea un socket y lo mantiene en escucha
+#constructor crea e inicializa un socket y lo mantiene en escucha
+
     def __init__(self):
+        self.soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print ("Socket Created")
+
+        self.soquete.bind((host, port))
+        print ("socket bind complete")
+        self.soquete.listen(1)
+        print ("socket now listening")
+
+#método que manda mensajes al cliente.
 
     def run(self):
+        while 1:
+            conn, addr = self.soquete.accept()
+            try:
+                print('conexion con {}.'.format(addr))
+                conn.send("server: Hello client".encode('UTF-8'))
+                while True:
+                     datos = conn.recv(4096)
+                     if datos:
+                         print('recibido: {}'.format(datos.decode('utf-8')))
+                     else:
+                         print("prueba")
+                         break
+            finally:
+                conn.close()
+
+S = chatserver()
+S = S.run()
