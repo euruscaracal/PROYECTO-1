1b4e5b84e126df3419e21ddd89d39cad6241fd93
Nicole Traschikoff <tricoleromina@ciencias.unam.mx>
2018-09-20 11:32:21 -0700

    Ahora ya puedo tener más de un cliente conectado al mismo tiempo

--- chatclient.py
+++ chatclient.py
@@ -1,28 +1,43 @@
 import socket
-
-host = "127.0.0.1"
-port = 6666
-
+import threading
+import pickle
 #clase que simula el cliente
 class chatclient:
 
-    def __init__(self):
 
-        self.soquete = socket.socket()
-        self.soquete.connect((host, port))
+    def __init__(self):
+        self.ip = str(input("Enter IP: "))
+        self.port = int(input("Enter port: "))
+        self.soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.soquete.connect((self.ip, self.port))
+
+    def send(self,msj):
+        while True:
+            self.soquete.send(pickle.dumps(msj))
+
+    def conexiones(self,msj):
+        self.hilo = threading.Thread(target=self.send, args=(msj,))
+        self.hilo.daemon = True
+        self.hilo.start()
 
     def run(self):
-        datos = self.soquete.recv(4096)
-        print (datos.decode('utf-8'))
 
         while True:
           message = input("envia un mensaje: ")
-          self.soquete.send(message.encode('utf-8'))
-
-          if message == "quit":
-            print("bye")
+          #self.soquete.send(pickle.dumps(message))
+          if message == "DISCONNECT":
+            print("")
             self.soquete.close()
             break
+          else:
+             self.conexiones(message)
+    def x(self):
+        while True:
+             datos = conn.recv(1024)
+             if datos:
+                 self.send(datos,conn)
 
-S = chatclient()
-S = S.run()
+
+if __name__ == "__main__":
+    S = chatclient()
+    S.run()
--- chatserver.py
+++ chatserver.py
@@ -1,39 +1,58 @@
 import socket
-
+import threading
+import pickle
+from threading import Thread
 #clase para simular un servidor
-host = "127.0.0.1"
-port = 6666
 
 class chatserver:
-
+    ip = "0.0.0.0"
+    port = 1234
 #constructor crea e inicializa un socket y lo mantiene en escucha
 
     def __init__(self):
+
         self.soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print ("Socket Created")
-
-        self.soquete.bind((host, port))
+        self.soquete.bind((self.ip, self.port))
         print ("socket bind complete")
-        self.soquete.listen(1)
+        self.soquete.listen()
         print ("socket now listening")
+        self.clientes = []
+
+    def conexiones(self):
+        self.hilo = threading.Thread(target=self.recibirmensaje)
+        self.hilo.daemon = True
+        self.hilo.start()
+
+
+    def send(self, mensaje, cliente):
+        for c in self.clientes:
+            if c != cliente:
+                self.c.send(mensaje)
+            else:
+                self.clientes.remove(c)
 
 #método que manda mensajes al cliente.
 
     def run(self):
-        while 1:
+        while True:
             conn, addr = self.soquete.accept()
-            try:
-                print('conexion con {}.'.format(addr))
-                conn.send("server: Hello client".encode('UTF-8'))
-                while True:
-                     datos = conn.recv(4096)
-                     if datos:
-                         print('recibido: {}'.format(datos.decode('utf-8')))
-                     else:
-                         print("prueba")
-                         break
-            finally:
-                conn.close()
+            self.soquete.setblocking(False)
+            self.clientes.append(conn)
+            hilo = threading.Thread(target=self.recibirmensaje(conn))
+            hilo.daemon = True
+            hilo.start()
+
+
+    def recibirmensaje(self,conn):
+        while True:
+             datos = conn.recv(1024)
+             if datos:
+                 self.send(datos,conn)
+             else:
+                 print("prueba")
+                 conn.close()
+                 break
 
 S = chatserver()
-S = S.run()
+S.run()
